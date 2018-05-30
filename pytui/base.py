83faec3b895edc2ad8d18793de436b54a8b07dcb
Emmanuel Arias <eamanu@eamanu.com>
2018-05-30 17:47:12 -0300

    add changes

--- pytui/base.py
+++ pytui/base.py
@@ -2,13 +2,14 @@ import curses
 
 
 class Terminal(object):
-
-    def __init__(self):
+    def not__init__(self):
         super(Terminal, self).__init__()
         self.__screen = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        self.set_border()
         self.__screen.keypad(True)
+        self.___hello_world()
 
     def kill_terminal(self):
         curses.nocbreak()
@@ -23,10 +24,29 @@ class Terminal(object):
             if c == ord('q'):
                 self.kill_terminal()
             self.__screen.addstr("Hello world\n")
+            self.__screen.refresh()
+            self.set_border()
 
-    def compile(self, initscr):
-        # compile the tui
+    def ___hello_world2(self, screen):
+        while 1:
+            c = screen.getch()
+            if c == ord('q'):
+                self.kill_terminal()
+            screen.addstr('Hello World!\n')
+            screen.refresh()
+            self.set_border(screen)        
+
+    def compile(self, stdscr):
+        stdscr.clear()
+        stdscr.refresh()
+        self.___hello_world2(stdscr)
+        
         return 0
 
     def run(self):
         curses.wrapper(self.compile)
+
+    def set_border(self, screen):
+        # self.__screen.border()
+        screen.border()
+        
